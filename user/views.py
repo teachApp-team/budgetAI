73fdd9b6074ade6f54c91203eaa772ca97ec383d
Hiroharu Hara <hh.25799623@gmail.com>
2020-11-01 15:43:49 +0900

    add CustomUser

--- user/views.py
+++ user/views.py
@@ -1,5 +1,10 @@
 from django.views import generic
+from .forms import SignupForm 
 
 # Create your views here.
 class IndexView(generic.TemplateView):
   template_name = 'index.html'
+
+class SignupView(generic.FormView):
+  template_name = 'signup.html'
+  form_class = SignupForm
