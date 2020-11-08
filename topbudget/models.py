cb093f188760a0c48feb91ffecf193fad81efedd
Hiroharu Hara <hh.25799623@gmail.com>
2020-11-08 18:11:23 +0900

    add picture column to spending model

--- topbudget/models.py
+++ topbudget/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Card(models.Model):
   name = models.CharField(verbose_name='決済手段名', max_length=20)
   date = models.DateField(verbose_name='引き落とし日', auto_now=False)
+  user = models.ForeignKey(CustomUser, verbose_name='ユーザー', on_delete=models.SET_NULL, null=True, blank=True)
 
   class Meta:
     verbose_name_plural = 'Card'
@@ -21,6 +22,7 @@ class Spending(models.Model):
   cash = models.BooleanField(verbose_name='現金決済')
   card = models.ForeignKey(Card, verbose_name='その他決済', on_delete=models.SET_NULL, null=True, blank=True)
   comment = models.CharField(verbose_name='コメント', max_length=40, null=True, blank=True)
+  picture = models.ImageField(upload_to='images/')
   created_at = models.DateTimeField(verbose_name='作成時', auto_now_add=True)
   updated_at = models.DateTimeField(verbose_name='更新日時', auto_now=True)
 
